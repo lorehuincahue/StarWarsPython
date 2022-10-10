a8ca42c029797e213e6de90adda1c4802b5c224f
lorehuincahue <lorehuincahue@gmail.com>
2022-10-10 18:33:03 +0000

    ok

--- src/models.py
+++ src/models.py
@@ -8,23 +8,42 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    last_name=Column(String(50), nullable=False)
+    email+Column(String(50), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Personajes(Base):
+    __tablename__ = 'personajes'
+    per_id = Column(Integer, primary_key=True)
+    per_name = Column(String(50), nullable=False)
+    status=Column(String(50), nullable=False)
+    species=Column(String(50), nullable=False)
+
+class Episodios(Base):
+    __tablename__ = 'episodios'
+    epi_id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    air_date=Column(String(50), nullable=False)
+    episode=Column(String(50), nullable=False)
+
+class Fav_Personajes(Base):
+    __tablename__='fav_personajes'
+    fav_per_id = Column(Integer, primary_key=True)
+    user_id = Column(String(50), ForeignKey ("user.user_id"))
+    per_id = Column(Integer, ForeignKey ("episodios.per_id"))
+    rel = relationship(Episodios)
+    rel= relationship (Personajes)
+
+class Fav_Episodios(Base):
+    __tablename__ = 'fav_episodios'
+    fav_epi_id = Column(Integer, primary_key=True)
+    user_id = Column(String(50), ForeignKey("user.user_id"))
+    char_ID = Column(Integer, ForeignKey("personajes.epi_id"))
 
     def to_dict(self):
         return {}
